855548423f02456f949f64b3423c441644924a5f
Bio_GD <tkismailov@edu.hse.ru>
2025-01-24 15:08:38 +0300

    Add lib

---
+++ gbo.hpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <array>
+
+using VEC_POP = std::array<double, 22>;
+
+class GBO{
+public:
+    int n;
+    int m;
+    int th;
+    double pr;
+
+    //std::vector<VEC_POP> vec(n, ); 
+};
